d5a9205277f3f80792a088e39d1d4817f27ac087
damxaq <damiancelico@gmail.com>
2021-10-25 00:02:06 +0200

    added some responsiveness

--- src/components/BoardColumn/BoardColumn.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import BoardColumn from "./BoardColumn";
-
-test("renders board", () => {
-  render(<BoardColumn />);
-});
--- src/components/TodoCard/TodoCard.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import TodoCard from "./TodoCard";
-
-test("renders todo card", () => {
-  render(<TodoCard />);
-});
